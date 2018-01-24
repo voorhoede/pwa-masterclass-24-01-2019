1106eb38598539d5020b855b5c7ce5ee0cc7488b
decrek <declanrek@gmail.com>
2018-01-24 13:35:34 +0100

    play around with service worker life cycle

--- src/service-worker.js
+++ src/service-worker.js
@@ -3,13 +3,16 @@
 // NOTE: rev hashed core assets from build will be included as global `var serviceworkerOption = { assets: [...] }` see https://www.npmjs.com/package/serviceworker-webpack-plugin
 
 self.addEventListener('install', event => {
-	return self.skipWaiting()
+	console.log('Installing service worasasddker');
+	return self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
+	console.log('Activating service worker');
 	return self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
-
+	console.log('Fetch event for:', event.request.url);
+	event.respondWith(new Response('Hijacked'));
 });
