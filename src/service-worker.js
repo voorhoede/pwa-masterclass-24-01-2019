dd62d5bc4b0eb7acd704d17cb269fc7ccc7b601d
decrek <declanrek@gmail.com>
2018-01-24 16:26:33 +0100

    add missing helper function

--- src/service-worker.js
+++ src/service-worker.js
@@ -43,3 +43,14 @@ function isCoreGetRequest(request) {
 function isHtmlGetRequest(request) {
 	return request.method === 'GET' && (request.headers.get('accept') !== null && request.headers.get('accept').indexOf('text/html') > -1);
 }
+
+/**
+ * Get a pathname from a full URL by stripping off domain
+ *
+ * @param {Object} requestUrl        The request object, e.g. https://www.mydomain.com/index.css
+ * @returns {String}                Relative url to the domain, e.g. index.css
+ */
+function getPathName(requestUrl) {
+	const url = new URL(requestUrl);
+	return url.pathname;
+}
